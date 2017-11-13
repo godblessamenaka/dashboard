613719e1e00ea2988b573b477ff756873e50a5a9
menakasampath <menaka.sampath@modaoperandi.com>
2017-11-13 13:52:40 -0500

    can load more per hour

--- src/app/recommendations.component.ts
+++ src/app/recommendations.component.ts
@@ -23,6 +23,7 @@ export class RecommendationsComponent {
 
 
 	ngOnChanges(): void {
+		this.fullRecs = [];
 		this.getRecs();
 		console.log(this);
 	}
@@ -41,14 +42,17 @@ export class RecommendationsComponent {
 		this.recs.slice(0, num).forEach((reco) => {
 			this.getRecInfo(reco);
 		});
+		this.recs = this.recs.slice(num,this.recs.length);
 	}
 
 	getRecInfo(rec): any {
 		var self = this;
+		
 		this.recommendationsService.getRecInfo(rec).then(res => {
 			self.fullRecs.push({
 				id: res.data.attributes.product_id, 
 				name: res.data.attributes.name,
+				price: res.data.attributes.retail_price_usd,
 				image: res.data.attributes.variants_data[0].primary_image_urls.small_url,
 				description: res.data.attributes.description,
 				url: res.data.attributes.variants_data[0].href
